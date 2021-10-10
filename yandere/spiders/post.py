2edad58aced991e7ae5b1dc51fee379e8a819ff1
Wen <wenslife@outlook.com>
2021-10-10 20:29:58 +0800

    Define PostSpider

---
+++ yandere/spiders/post.py
@@ -0,0 +1,25 @@
+import scrapy
+
+from yandere.items import PostItem
+
+
+class PostSpider(scrapy.Spider):
+    """
+    Post List Spider
+    limit: How many posts you want to retrieve. There is a hard limit of 100 posts per request.
+    page: The page number.
+    tags: The tags to search for.
+    """
+    name = 'post'
+    allowed_domains = ['yande.re']
+
+    def __init__(self, limit=100, page=1, tags='', **kwargs):
+        super().__init__(**kwargs)
+        self.start_urls = [f'https://yande.re/post.json?limit={limit}&page={page}&tags={tags}']
+
+    def parse(self, response, **kwargs):
+        posts = response.json()
+        for post in posts:
+            item = PostItem()
+            item['file_url'] = post['file_url']
+            yield item
