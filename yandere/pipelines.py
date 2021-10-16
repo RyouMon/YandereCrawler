296f3fd41e323360085e104728b0c8e25404946b
Wen <wenslife@outlook.com>
2021-10-16 10:56:18 +0800

    feat(pipelines): rewrite YandereImagePipeline.get_media_requests()

--- yandere/pipelines.py
+++ yandere/pipelines.py
@@ -5,11 +5,15 @@
 
 
 # useful for handling different item types with a single interface
+import scrapy
 from urllib.parse import unquote
 from scrapy.pipelines.images import ImagesPipeline
 
 
 class YandereImagesPipeline(ImagesPipeline):
+
+    def get_media_requests(self, item, info):
+        return scrapy.Request(item['file_url'])
 
     def file_path(self, request, response=None, info=None, *, item=None):
         # Using original filename
